e40a969ad632e5beb69f944f88d1a4c4dc837696
Jinderamarak <47433483+Jinderamarak@users.noreply.github.com>
2025-03-10 12:04:51 +0100

    feat: setup go server

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func main() {
+	server := gin.Default()
+	server.Use(CORSMiddleware())
+	server.GET("/game/:id", gameHandler)
+	errors.Unwrap(server.Run("localhost:8080"))
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
+func gameHandler(ctx *gin.Context) {
+	gameId := ctx.Param("id")
+
+	conn, _ := upgrader.Upgrade(ctx.Writer, ctx.Request, nil)
+	defer conn.Close()
+
+	conn.WriteMessage(websocket.TextMessage, []byte("game: "+gameId))
+}
